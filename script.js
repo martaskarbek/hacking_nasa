750b4ebe4c36427eba5bcb91ab1d58de15b97645
Marta Skarbek <marta@skarbek.name>
2020-07-12 16:07:44 +0200

    refactored to add change to dark theme part one

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 const themeChanger = document.querySelector('button');
 const page = document.querySelector('body');
-const text = document.querySelector('.button');
-
+const buttons = document.querySelectorAll('.button'); // NodeList (możemy użyć konstrukcji forEach)
 let bcg_color = 'black';
 let font_color = 'white';
 
-function changeTheme() {
-    page.style.backgroundColor = bcg_color;
-    page.style.color = font_color;
-    text.style.color = font_color;
-    console.log("dupa");
+function init() {
+    // check if darkMode is not set, then is eqal false
+    let darkMode = localStorage.getItem("darkMode");
+    darkMode = darkMode === 'false' ? false : true
+
+    if(themeChanger !== null)
+        themeChanger.addEventListener('click', changeTheme);
+
+    if(!darkMode) return;
+    console.log('%c change style ', 'color: blue;')
+    page.classList.toggle('dark-background');
+    buttons.forEach(button => button.classList.toggle('dark-text'));
 }
 
-themeChanger.addEventListener('click', changeTheme)
+function changeTheme() {
+    page.classList.toggle('dark-background');
+    buttons.forEach(button => button.classList.toggle('dark-text'));
+    themeChanger.classList.toggle('dark-button');
+
+    let darkMode = localStorage.getItem("darkMode");
+    darkMode = darkMode === 'false' ? false : true
+    console.log(darkMode);
+    localStorage.setItem("darkMode", !darkMode);
+}
+
+init();
